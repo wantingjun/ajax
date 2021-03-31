c69626639f6b395294203f9ae72a7521effc1eb4
TingjunWan <wtj9631@163.com>
2021-04-01 07:24:05 +0800

    HTML,XML,JSON,page

--- public/main.js
+++ public/main.js
@@ -1,4 +1,68 @@
+// const { getMaxListeners } = require("node:process")
+let n =1;
 console.log('i am main')
+getPage.onclick=()=>{
+    const request = new XMLHttpRequest()
+    request.open('get',`/page${n + 1}`)
+    request.onreadystatechange=()=>{
+        console.log(request.response)
+        if(request.readyState ===4 && request.status ===200){
+            const array = JSON.parse(request.response)
+            array.forEach(item =>{
+                const li = document.createElement("li");
+                li.textContent =item.id
+                liId.appendChild(li)
+
+            })
+           n =  n+1
+        }
+    }
+    request.send()
+}
+getJSON.onclick=()=>{
+    const request = new XMLHttpRequest()
+    request.open('get','/5.JSON')
+    request.onreadystatechange=()=>{
+        if(request.readyState === 4 && request.status ===200){
+            console.log(request.response)
+            const object = JSON.parse(request.response)
+            console.log(object)
+            showname.innerHTML= object.name
+        }
+    }
+    request.send()
+}
+getXML.onclick=()=>{
+    const request = new XMLHttpRequest();
+    request.open('GET','/4.xml')
+    request.onreadystatechange=()=>{
+        if( request.readyState ===4 && request.status===200){
+            console.log(request.responseXML)
+            const dom = request.responseXML
+            const text = dom.getElementsByTagName('warning')[0].textContent
+            console.log(text.trim())
+        }
+
+    }
+    request.send()
+}
+getHTML.onclick =()=>{
+    const request = new XMLHttpRequest()
+    request.open('get','/3.html')
+    request.onload =() =>{
+        console.log(request.response)
+        // 创建标签
+        const div = document.createElement('div')
+        // 填写内容
+        div.innerHTML = request.response
+        // 插入body
+        document.body.appendChild (div)
+
+    }
+    request.onerror = () =>{}
+    request.send()
+
+}
 getJS.onclick =()=>{
     const request = new XMLHttpRequest()
     request.open('get','/2.js')
@@ -20,14 +84,28 @@ getJS.onclick =()=>{
 getCss.onclick=()=>{
     const request = new XMLHttpRequest()
     request.open('get','/style.css')
-    request.onload=()=>{
-        console.log('success')
-        const style = document.createElement('style')
-        style.innerHTML = request.response
-        document.head.appendChild(style)
-    }
-    request.onerror=()=>{
-        console.log('fail')
+    // request.onload=()=>{
+    //     console.log('success')
+    //     const style = document.createElement('style')
+    //     style.innerHTML = request.response
+    //     document.head.appendChild(style)
+    // }
+    // request.onerror=()=>{
+    //     console.log('fail')
+    // }
+    request.onreadystatechange=()=>{
+        console.log(request.readyState)
+        if(request.readyState === 4){ //无法判断是成功还是失败的下载完
+            console.log('下载完成')
+            if(request.status >=200 && request.status <= 300){ //成功的下载完成
+                const style = document.createElement('style')
+                style.innerHTML = request.response
+                document.head.appendChild(style)
+            }
+            else{
+                console.log('加载css失败')
+            }
+        }
     }
     request.send()
 }
--- server.js
+++ server.js
@@ -24,7 +24,12 @@ var server = http.createServer(function(request, response){
   if(path === '/index.html'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(fs.readFileSync('public/index.html'))
+    let string = fs.readFileSync('public/index.html').toString()
+    let page1 = fs.readFileSync('db/page1.json').toString()
+    const array = JSON.parse(page1)
+    const result = array.map(item =>`<li >${item.id}</li>`).join('')
+    string = string.replace('{{page}}',`<ul id="liId">${result}</ul>`)
+    response.write(string)
     response.end()
   } else if(path === '/main.js'){
     response.statusCode = 200
@@ -38,6 +43,36 @@ var server = http.createServer(function(request, response){
     response.write(fs.readFileSync('public/2.js'))
     response.end()
   } 
+  else if(path === '/3.html'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(fs.readFileSync('public/3.html'))
+    response.end()
+  } 
+  else if(path === '/4.xml'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/xml;charset=utf-8')
+    response.write(fs.readFileSync('public/4.xml'))
+    response.end()
+  }
+  else if(path === '/5.JSON'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/json;charset=utf-8')
+    response.write(fs.readFileSync('public/5.JSON'))
+    response.end()
+  }
+  else if(path === '/page2'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/json;charset=utf-8')
+    response.write(fs.readFileSync('db/page2.json'))
+    response.end()
+  }
+  else if(path === '/page3'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/json;charset=utf-8')
+    response.write(fs.readFileSync('db/page3.json'))
+    response.end()
+  }
   else if(path ==='/style.css'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
